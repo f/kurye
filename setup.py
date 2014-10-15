1979f4fde61acd437259cc0d0c19db6367c352c2
f <fka@fatihak.in>
2014-10-15 13:06:04 +0300

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='kurye',
-  version='0.0.1.dev1',
+  version='0.0.1a1',
   description='simple github cloner for boilerplate projects',
   url='https://github.com/f/kurye',
   author='Fatih Kadir Akin',
